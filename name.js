05afe5d2378139410e30c25244dd15960b8b6f29
Bandemb <83180975+Bandemb@users.noreply.github.com>
2021-04-26 01:31:35 +0545

    Add files via upload

---
+++ name.js
@@ -0,0 +1,19 @@
+function dome() {
+    document.getElementById('selection').style.display = 'block';
+
+}
+
+function dome2() {
+    document.getElementById('selection').style.display = 'none';
+
+}
+
+function dome3() {
+    document.getElementById('selection2').style.display = 'block';
+
+}
+
+function dome4() {
+    document.getElementById('selection2').style.display = 'none';
+
+}
